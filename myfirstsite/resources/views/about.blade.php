77aa5ab0551d3a8769272e6d15f4f49ae0dca927
Benjamin H Boruff <ben@boruff.me>
2019-01-27 19:17:26 -0500

    added layout and about, contact views and routes

---
+++ myfirstsite/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>
+    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad alias, amet, aperiam atque blanditiis
+        cupiditate delectus earum eius est eum explicabo incidunt laboriosam quas quibusdam quidem quos
+        repellat temporibus.</span>
+        <span>Corporis deserunt, dolorem dolores et eveniet laudantium neque odio optio qui quo quod repudiandae sed sint
+        soluta totam veritatis vero! Aperiam, eligendi explicabo labore laudantium quasi repellat tenetur?
+        Consequuntur, excepturi?</span>
+        <span>Ad atque cumque dolore ducimus enim error ex exercitationem expedita facere fuga hic, impedit in ipsa laborum
+        mollitia officia possimus quam quas ratione recusandae repellendus repudiandae ullam veniam vero
+        voluptatum.</span>
+        <span>Animi beatae dolorum eius exercitationem expedita harum ipsam iste libero! Amet, atque ducimus earum et
+        explicabo impedit magni molestias obcaecati perspiciatis possimus quae quo recusandae similique. A dolorum qui
+        saepe!</span>
+        <span>Adipisci aliquid architecto at corporis dolor dolore dolorum ea exercitationem ipsam laudantium magni maxime,
+        mollitia natus nemo neque obcaecati optio quia repellendus reprehenderit sunt tempore tenetur vitae voluptas
+        voluptates voluptatum.</span>
+    </p>
+@endsection
---
+++ myfirstsite/resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Contact')
+
+@section('content')
+    <h1>Contact Form</h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more.
+    </p>
+@endsection
