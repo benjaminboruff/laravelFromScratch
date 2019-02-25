79f787c2279de96056b4fd912543b946eee05d3f
Benjamin H Boruff <ben@boruff.me>
2019-02-24 20:24:51 -0500

    movin on

---
+++ myfirstsite/app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        return view('welcome', [
+            'tasks' => ['adsasd', 'adsasda'],
+            'foo' => 'foobar'
+        ]);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
--- myfirstsite/routes/web.php
+++ myfirstsite/routes/web.php
@@ -11,15 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('contact', function () {
-    return view('contact');
-});
-
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('/', 'PagesController@index');
+Route::get('about', 'PagesController@about');
+Route::get('contact', 'PagesController@contact');
+Route::get('projects', 'ProjectsController@index');
 
